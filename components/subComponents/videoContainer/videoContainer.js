25b0432582e2d5f5a32ac2681ff612c45c100614
Chris McNeese <christophermcneese@mac.com>
2022-03-30 13:35:42 -0500

    add background image to site

--- components/subComponents/videoContainer/videoContainer.js
+++ components/subComponents/videoContainer/videoContainer.js
@@ -5,6 +5,9 @@ const StyledVideoContainer = styled.div`
   max-width: 800px;
   margin: 0 auto;
   padding:9.25em 0 0;
+  background-image: url("bg01_homepage.jpg");
+  background-position: center;
+  // height: 500px;
 `;
 
 const VideoContainer = (props) => (
