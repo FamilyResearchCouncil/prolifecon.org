ca7dcf63b9e882e6171879fa17f4ccbe003e8bd6
Chris McNeese <christophermcneese@mac.com>
2022-07-20 15:50:22 -0500

    save work on Donate button component

---
+++ components/subComponents/DonateButton/donateButton.js
@@ -0,0 +1,9 @@
+import {StyledDonateButton} from "./donateButton.styled";
+
+const DonateButton = (props) => (
+    <StyledDonateButton href={props.href}>
+
+    </StyledDonateButton>
+);
+
+export default DonateButton;
